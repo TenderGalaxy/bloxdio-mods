e37fdc7dcdd7b906fc3bf1349f279bd9d4ea81f9
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-11-25 19:53:47 -0800

    Create runtime_tester.js

---
+++ runtime_tester.js
@@ -0,0 +1,51 @@
+/*
+Runtime tester: Check the amount of time it takes to run code in microseconds
+2025
+*/
+
+`
+Theory: Add operations until it halts at 5000 IU
+`
+tu = {
+  a: [0],
+  func(){
+    return 0
+  },
+  step1(){
+    while(1){;}
+  },
+  step2(){
+    tu.func()
+    tu.i = 1000
+    tu.interruptions = tu.i * 3 + 6
+    while(tu.i--){tu.a.copyWithin(0,1)}
+    while(true){tu.interruptions++}
+  },
+  step3(){
+    tu.func()
+    tu.i = 4999 - tu.interruptions
+    while(tu.i--){;}
+    tu.t = 0
+    while(true){
+      tu.i = 4999
+      while(tu.i--){;}
+      tu.t++
+    }
+  },
+  get(){
+    return tu.t
+  }
+}
+for(let i = 0; i < 12; i++){
+  tu.a = tu.a.concat(tu.a)
+}
+tu.ex = 'tu.t=0,tu.t++' + ',tu.t++'.repeat(101000)
+/*
+Code Block 1:
+tu.func = (FUNCTION)
+tu.init()
+Code Block 2:
+(Run Code)
+tu.measure()
+Code Block 3:
+*/
